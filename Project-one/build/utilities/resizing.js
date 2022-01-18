0c3942ac4b6a5ee862586d4afb445110074de755
Shikovich95 <Shikovich95>
2022-01-18 20:09:24 +0200

    caching is finished using fs and exif-parser

--- Project-one/build/utilities/resizing.js
+++ Project-one/build/utilities/resizing.js
@@ -1,31 +1,56 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.welcome = exports.imgResize = void 0;
 const queryString = require('querystring');
 const path = require('path');
 const sharp = require('sharp');
+//import QueryString from 'qs';
+const fs_1 = __importDefault(require("fs"));
+const ts_exif_parser_1 = require("ts-exif-parser");
 const welcome = (req, res) => {
     res.send('Hello from routes!');
 };
 exports.welcome = welcome;
 const imgResize = (req, res) => {
-    /*  const QueryData:Quering = {
-       fileName: req.query.filname,
-       width: req.query.width,
-       height: req.query.filename
-
-     } */
+    // get the query data from the url 
     const QueryData = req.query;
-    let ImgWidth = QueryData.width;
-    let ImgHeight = QueryData.height;
-    const filename = QueryData.filename, imgW = parseInt(ImgWidth), imgH = parseInt(ImgHeight), originalImg = path.join(__dirname, `../static/imgs/${filename}`);
+    let ImgWidth = QueryData.width, ImgHeight = QueryData.height, fileName = QueryData.filename;
+    const filename = fileName, imgW = parseInt(ImgWidth), imgH = parseInt(ImgHeight), originalImg = path.join(__dirname, `../static/imgs/${filename}`), newImg = path.join(__dirname, `../static/imgs/thumb-${filename}`);
+    /*
+      * function to resize the img with the requested dimension using sharp module
+      * it takes the name and the path of the img using path module (look at originalImg variable)
+      * send the resizedImg to the user using res.sendFile
+    */
+    const resizeImg = (img) => {
+        sharp(img).resize(imgW, imgH).toFile(path.join(__dirname, `../static/imgs/thumb-${filename}`), (err, info) => { });
+        setTimeout(() => { res.sendFile(`thumb-${filename}`, { root: path.join(__dirname, '../static/imgs') }); }, 500);
+        console.log('newly resized img is sent');
+    };
     // function resizing the image using sharp module and it is self invoked
-    (function () {
+    (() => {
+        var _a, _b;
+        try {
+            const readImg = fs_1.default.readFileSync(newImg), 
+            // using exif-parser module to get the meta data of the img and the compare it for the requested one
+            parser = ts_exif_parser_1.ExifParserFactory.create(readImg), imgData = (parser.parse());
+            const preW = (_a = imgData.imageSize) === null || _a === void 0 ? void 0 : _a.width, preH = (_b = imgData.imageSize) === null || _b === void 0 ? void 0 : _b.height;
+            if (preH != imgH || preW != imgW) {
+                resizeImg(originalImg);
+            }
+            else {
+                res.sendFile(`thumb-${filename}`, { root: path.join(__dirname, '../static/imgs') });
+                console.log('previous img has been sent');
+            }
+        }
+        catch (err) {
+            console.log(`${err} \n no thumbnail img found`);
             // sharp takes the img name then apply the resize method on it that takes two params width and height whivh I got from query url
-            sharp(originalImg).resize(imgW, imgH).toFile(path.join(__dirname, `../static/imgs/new${filename}`), (err, info) => {
-            });
-        })();
-    // Using setTimeout as res.sendFile is async so it will send file before it processed 
-    setTimeout(() => { res.sendFile(`new${filename}`, { root: path.join(__dirname, '../static/imgs') }); }, 500);
+            resizeImg(originalImg);
+            // Using setTimeout as res.sendFile is async so it will send file before it processed 
+        }
+    })();
 };
 exports.imgResize = imgResize;
--- Project-one/src/utilities/resizing.ts
+++ Project-one/src/utilities/resizing.ts
@@ -1,8 +1,11 @@
 const queryString  = require('querystring');
 const path = require('path');
 const sharp = require('sharp');
-import {Request, Response} from 'express'
-import QueryString from 'qs';
+import {Request, Response} from 'express';
+//import QueryString from 'qs';
+import fs,{promises as fsPromise}   from 'fs';
+import { buffer } from 'stream/consumers';
+import {ExifParserFactory}  from 'ts-exif-parser';
 
 
 const welcome = (req:Request, res:Response) => {
@@ -10,30 +13,55 @@ const welcome = (req:Request, res:Response) => {
      };
 
 const imgResize =  (req: Request, res: Response) => {
-     /*  const QueryData:Quering = {
-        fileName: req.query.filname,
-        width: req.query.width,
-        height: req.query.filename
-
-      } */
+      // get the query data from the url 
       const QueryData = req.query;
-      
       let ImgWidth:unknown = QueryData.width , ImgHeight:unknown = QueryData.height,fileName:unknown = QueryData.filename;
-
       const filename:string = fileName as string, imgW:number = parseInt(ImgWidth as string),  imgH:number = parseInt(ImgHeight as string),
-      originalImg= path.join(__dirname ,`../static/imgs/${filename}`);
-   
-      // function resizing the image using sharp module and it is self invoked
-      (() => {
-        // sharp takes the img name then apply the resize method on it that takes two params width and height whivh I got from query url
-         sharp(originalImg).resize(imgW,imgH).toFile(path.join(__dirname ,`../static/imgs/new${filename}`),(err:object,info:object) => {
-         })
-       })()
-       // Using setTimeout as res.sendFile is async so it will send file before it processed 
-       setTimeout( ()=> {res.sendFile(`new${filename}`, { root: path.join(__dirname, '../static/imgs') })}, 500)
+      originalImg= path.join(__dirname ,`../static/imgs/${filename}`), newImg = path.join(__dirname ,`../static/imgs/thumb-${filename}`);
+      /*
+        * function to resize the img with the requested dimension using sharp module 
+        * it takes the name and the path of the img using path module (look at originalImg variable)
+        * send the resizedImg to the user using res.sendFile 
+      */
+      const resizeImg =   (img:string) => {
+        sharp(img).resize(imgW,imgH).toFile(path.join(__dirname ,`../static/imgs/thumb-${filename}`),(err:object,info:object) => {} )
+        setTimeout( ()=> {res.sendFile(`thumb-${filename}`, { root: path.join(__dirname, '../static/imgs') })}, 500)
+        console.log('newly resized img is sent')
+      }
+
+        // function resizing the image using sharp module and it is self invoked
+        ( () => {
+          try{
+            const readImg = fs.readFileSync(newImg),
+            // using exif-parser module to get the meta data of the img and the compare it for the requested one
+            parser = ExifParserFactory.create(readImg),
+            imgData = (parser.parse());
+            const preW = imgData.imageSize?.width, preH = imgData.imageSize?.height;
+            if (preH != imgH || preW != imgW){
+              resizeImg(originalImg);
+            }
+            else{
+              res.sendFile(`thumb-${filename}`, { root: path.join(__dirname, '../static/imgs') });
+              console.log('previous img has been sent')
+            }
+          
+          }
+          catch(err){
+            console.log(`${err} \n no thumbnail img found`)
+            
+            // sharp takes the img name then apply the resize method on it that takes two params width and height whivh I got from query url
+            resizeImg(originalImg);
+            
+            // Using setTimeout as res.sendFile is async so it will send file before it processed 
+           
+          }
+            
+          })();
+          
     }
 
   
 
 
   export {imgResize , welcome} ;
+
